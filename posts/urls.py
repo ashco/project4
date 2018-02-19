773fb92e10caf9243545968c667d481a88390a76
Ashton <ashchristie@gmail.com>
2018-02-19 00:02:42 -0800

    starting project

---
+++ posts/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index') # [r = route][^ = start with][$ = End with], checks in views.py file for method called index
+]
