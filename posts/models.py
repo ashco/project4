974ea3c3793e56bc50ad25188e7e21940ced4943
Ashton <ashchristie@gmail.com>
2018-02-19 21:28:39 -0800

    added in image upload, started messing around with tests

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,16 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField( default=datetime.now, blank = True )
+    image = models.FileField(upload_to='post_image', blank=True)
+
     def __str__(self):
         return self.title #show title in admin page
     # class Meta:
     #     verbose_name_plural = 'Posts' #makes posts database singular
+
+class Author(models.Model):
+    author = models.CharField(max_length=64)
+    author_image = models.FileField(upload_to='author_image', blank=True)
+    
+    def __str__(self):
+        return self.author #show title in admin page
--- project4/urls.py
+++ project4/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^$', include('posts.urls')),
     path('admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
